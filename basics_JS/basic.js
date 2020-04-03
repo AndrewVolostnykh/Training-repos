e54c5674186c6c4ad144a59574f5923e484ca375
Andrew Volostnykh <zighalt790@gmail.com>
2020-04-03 17:09:08 +0300

    Changing color and texn on click or mouse over using JS

--- basics_JS/basic.js
+++ basics_JS/basic.js
@@ -20,3 +20,58 @@ function inputUser()
 	}
 	document.getElementById("result").innerHTML = "Name: " + user.name + ", surname: " + user.surname + ", age: " + user.age;
 }
+
+function changeColor(obj)
+{
+	var currnetColor = getComputedStyle(document.getElementById('color')).backgroundColor;
+	if(currnetColor == 'rgb(102, 153, 255)')
+	{
+		obj.style.backgroundColor = '#cc66ff';
+	} else {
+		obj.style.backgroundColor = '#6699ff';
+	}
+}
+
+function changeText(obj)
+{
+	var currentText = document.getElementById("text").textContent;
+	if(currentText == 'Change text')
+	{
+		obj.innerHTML = "Nice! it Changed!";
+	} else {
+		obj.innerHTML = "Change text";
+	}
+}
+
+function changeTextColor(obj)
+{
+	var text = document.getElementById("colorText").textContent;
+	var color = getComputedStyle(document.getElementById('colorText')).backgroundColor;
+
+	if(text == "Change color and text")
+	{
+		obj.innerHTML = "Okey, it changed!";
+	} else {
+		obj.innerHTML = "Change color and text";
+	}
+
+	if(color == 'rgb(102, 153, 255)')
+	{
+		obj.style.backgroundColor = '#cc66ff';
+	} else {
+		obj.style.backgroundColor = '#6699ff';
+	}
+}
+
+function mouseOver(obj)
+{
+	obj.innerHTML = "NICE!";
+	obj.style.backgroundColor = '#cc0066';
+}
+
+function mouseOut(obj)
+{
+	obj.innerHTML = "Mouser over me!!!";
+	obj.style.backgroundColor = '#ff6699'
+
+}
