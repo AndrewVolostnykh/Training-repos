f7cdbec9c646882291016746cb9a2050379f723c
Andrew Volostnykh <zighalt790@gmail.com>
2020-04-24 15:50:47 +0300

    web site going to have nice view, added header and footer.

--- ReactCoursera/my-app/src/components/MainComponent.js
+++ ReactCoursera/my-app/src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 class Main extends Component {
 
@@ -22,14 +23,11 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Restorante ConFusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
         <Menu dish={this.state.dishes}
             onClick={(dishId) => this.onDishSelect(dishId)} />
         <Dishdetail selectedDish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Footer />
       </div>
     );
   }
