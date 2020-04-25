da945e8618561d83581deb91cb19a679b943facc
Andrew Volostnykh <zighalt790@gmail.com>
2020-04-26 01:53:09 +0300

    single page architecture used for app. Added home page with best dish, promotion and chief

--- ReactCoursera/my-app/src/components/MainComponent.js
+++ ReactCoursera/my-app/src/components/MainComponent.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+
+
 
 class Main extends Component {
 
@@ -14,6 +20,9 @@ class Main extends Component {
 
     this.state = {
       dishes : DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
       selectedDish: null
     };
   }
@@ -26,7 +35,9 @@ class Main extends Component {
   render() {
     const HomePage = () =>{
       return(
-        <Home />
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
       );
     }
 
@@ -36,6 +47,7 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dish={this.state.dishes}/>} />
+          <Route exact path='/contactus' component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
